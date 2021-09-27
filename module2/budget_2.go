ca974a3ffd015a07b30115a8ed215fa975e870fd
Gregory Roberts <roberts.gregory@heb.com>
2021-09-27 13:34:32 -0500

    Create a new budget

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -74,6 +74,9 @@ func (b *Budget) RemoveItem(description string) {
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
+	b := Budget{Max: max}
+	newBudget = &b
+	report[month] = newBudget
 
 	return newBudget, nil
 }
